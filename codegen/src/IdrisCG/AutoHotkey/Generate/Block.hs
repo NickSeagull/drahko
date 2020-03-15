c2a2b5b21227380c75133c4858622b68265a6e95
Nikita Tchayka <nick@booster.cloud>
2020-03-15 16:50:11 +0000

    update

--- codegen/src/IdrisCG/AutoHotkey/Generate/Block.hs
+++ codegen/src/IdrisCG/AutoHotkey/Generate/Block.hs
@@ -61,10 +61,10 @@ foreign' _ (Idris.FCon name) params =
   case (show name, params) of
     (other, p) ->
       error ("\nForeign function not supported\n\n\t" <> show other <> " " <> show p)
-foreign' returning (Idris.FApp name params) _ =
+foreign' returning (Idris.FApp name params) others =
   case (show name, params) of
     (other, p) ->
-      error ("\nForeign function not supported\n\n\t" <> show other <> " " <> show p)
+      error ("\nForeign function not supported\n\n\t" <> show other <> " " <> show p <> "\n\n\t" <> show others)
 foreign' _ f _ = error ("Foreign function not supported: " <> show f)
 
 conditionals :: (Expression -> Statement) -> Expression -> [Idris.SAlt] -> Block

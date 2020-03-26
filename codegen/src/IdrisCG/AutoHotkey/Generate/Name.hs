7597db131147cf957523eccf6f46025b25fd3a90
Nikita Tchayka <nick@booster.cloud>
2020-03-26 13:50:35 +0000

    Stop Z-encoding
    It was unnecessary as, in reality, Idris doesn't put much weird
    symbols apart from braces.

--- codegen/src/IdrisCG/AutoHotkey/Generate/Name.hs
+++ codegen/src/IdrisCG/AutoHotkey/Generate/Name.hs
@@ -3,12 +3,15 @@ module IdrisCG.AutoHotkey.Generate.Name where
 import qualified Idris.Core.TT as Idris (Name, showCG)
 import IdrisCG.AutoHotkey.Syntax (Name (..))
 import Relude
-import Text.Encoding.Z (zEncodeString)
+
+notAllowedSymbols :: String
+notAllowedSymbols = "{}[]()"
 
 generate :: Idris.Name -> Name
 generate idrisName = do
-  let encodedName = zEncodeString (Idris.showCG idrisName)
-  Name ("idris_" <> toText encodedName)
+  let removeSymbols = filter (`notElem` notAllowedSymbols)
+  let encodedName = Idris.showCG idrisName & removeSymbols
+  Name (toText encodedName)
 
 loc :: Int -> Name
 loc i = Name ("loc" <> show i)

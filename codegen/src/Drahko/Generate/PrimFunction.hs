8446e7969c58e6e12951fe2de0eb7735c558c1f9
Nick Tchayka <nick@booster.cloud>
2020-03-28 17:23:44 +0000

    perf: Minify primitive functions (#13)

--- codegen/src/Drahko/Generate/PrimFunction.hs
+++ codegen/src/Drahko/Generate/PrimFunction.hs
@@ -2,16 +2,20 @@ module Drahko.Generate.PrimFunction where
 
 import Drahko.Generate.Common
 import qualified Drahko.Generate.Name as Name
-import Drahko.Syntax (Expression (..))
+import Drahko.Syntax
 import qualified IRTS.Lang as Idris (PrimFn (..))
 import Relude
 
 generate :: Idris.PrimFn -> [Expression] -> Expression
 generate Idris.LWriteStr [_, str] =
-  Apply (Variable "idris_putStr") [str]
+  Apply (primitiveFunction "putStr") [str]
 generate (Idris.LExternal n) params =
   Apply (thisDot $ Variable $ Name.generate n) params
 generate Idris.LCrash args =
-  Apply (Variable "idris_crash") args
+  Apply (primitiveFunction "crash") args
 generate x _ =
   error ("\nPrimitive function not implemented \n\n\t" <> show x <> "\n")
+
+primitiveFunction :: Text -> Expression
+primitiveFunction nameText =
+  DotAccess (Variable "__drahko") (Variable $ Name nameText)
